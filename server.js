70a7bbefad9330c523346b21d229c6ad3d9b1b23
JagatheswariManoharan <jagatheswarimanoharan@dhcp-v121-001.mobile.uci.edu>
2019-05-15 18:29:00 -0700

    made changes

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const { join } = require('path')
-const app = express()
+// const express = require('express')
+// const { join } = require('path')
+// const app = express()
 
-app.use(express.static(join(__dirname, '/public')))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.static(join(__dirname, '/public')))
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
-require('./routes')(app)
+// require('./routes')(app)
 
-require('./config').connect(_ => app.listen(3000))
+// require('./config').connect(_ => app.listen(3000))
+const http = require('http')
+const PORT = 8080
+function handleRequest (req, res) {
+  res.end('It Works!! Path Hit: ' + req.url)
+}
+const server = http.createServer(handleRequest)
+server.listen(PORT, _ => {
+ console.log('Server listening on: http://localhost:' + PORT)
+})
